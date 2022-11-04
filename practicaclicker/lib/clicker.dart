cf77db0d11fc776fd406e974a07d852e10e0687c
Pablodev21 <108357129+Pablodev21@users.noreply.github.com>
2022-11-04 11:33:43 +0100

    cambio en clase

--- practicaclicker/lib/clicker.dart
+++ practicaclicker/lib/clicker.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:footer/footer.dart';
+import 'package:footer/footer_view.dart';
 
 import 'main.dart';
 
@@ -23,63 +25,67 @@ class StatesAppState extends State<StatesApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: Drawer(
-            //Borde redondeado
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(20),
-                  bottomRight: Radius.circular(20)),
-            ),
-            child: ListView(children: [
-              DrawerHeader(
-                child: Container(
-                    alignment: Alignment.center,
-                    child: Icon(
-                      Icons.home,
-                      size: 70,
-                    )),
-              ),
-              ListTile(
-                title: Text("Hola pablo"),
-                leading: Icon(Icons.integration_instructions),
-              ),
-              ListTile(
-                title: Text("Item 2"),
-                leading: Icon(Icons.integration_instructions),
-              ),
-              ListTile(
-                title: Text("Item 3"),
-                leading: Icon(Icons.integration_instructions),
-              ),
-              ListTile(
-                title: Text("Item 1"),
-                leading: Icon(Icons.integration_instructions),
-              ),
-            ])),
-        appBar: AppBar(
-            title: Center(child: Text("Clicker", textAlign: TextAlign.center))),
-        //Explicacion: ya teniendo controlado el index de los items del navigationsBar he creado una lista de Widgets, los cuales estan definidos abajo,
-        //entonces, imaginemonos que queremos utilizar el item1, lo unico que tenemos que hacer el clickar en el item1
-        //se guardara el index de este en la variable indexItem dsp se llama en un container a la lista pasandole el indexItem, esto se correspondera al Widget Item1
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                child: Text("Mundo 1"),
-              ),
-              Container(
-                child: Text("Imagen"),
-              ),
-              Container(child: Text("Barra de vida")),
-              Container(
-                child: Text("Monedas"),
-              ),
-              Container(
-                child: Scroll(),
-              )
-            ],
+      drawer: Drawer(
+          //Borde redondeado
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20),
+                bottomRight: Radius.circular(20)),
           ),
-        ));
+          child: ListView(children: [
+            DrawerHeader(
+              child: Container(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    Icons.home,
+                    size: 70,
+                  )),
+            ),
+            ListTile(
+              title: Text("Hola pablo"),
+              leading: Icon(Icons.integration_instructions),
+            ),
+            ListTile(
+              title: Text("Item 2"),
+              leading: Icon(Icons.integration_instructions),
+            ),
+            ListTile(
+              title: Text("Item 3"),
+              leading: Icon(Icons.integration_instructions),
+            ),
+            ListTile(
+              title: Text("Item 1"),
+              leading: Icon(Icons.integration_instructions),
+            ),
+          ])),
+      appBar: AppBar(
+          title: Center(child: Text("Clicker", textAlign: TextAlign.center))),
+      //Explicacion: ya teniendo controlado el index de los items del navigationsBar he creado una lista de Widgets, los cuales estan definidos abajo,
+      //entonces, imaginemonos que queremos utilizar el item1, lo unico que tenemos que hacer el clickar en el item1
+      //se guardara el index de este en la variable indexItem dsp se llama en un container a la lista pasandole el indexItem, esto se correspondera al Widget Item1
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              child: Text(
+                "Mundo 1",
+                textAlign: TextAlign.right,
+              ),
+            ),
+            Container(
+              child: imagenClick(),
+            ),
+            Container(child: Text("Barra de vida")),
+            Container(
+              child: Text("Monedas"),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Container(
+        child: Scroll(),
+      ),
+    );
   }
 }
 
@@ -113,9 +119,42 @@ class Scroll extends StatelessWidget {
             width: 160.0,
             color: Colors.orange,
           ),
+          Container(
+            width: 160.0,
+            color: Colors.red,
+          ),
+          Container(
+            width: 160.0,
+            color: Colors.blue,
+          ),
+          Container(
+            width: 160.0,
+            color: Colors.orange,
+          ),
+          Container(
+            width: 160.0,
+            color: Colors.green,
+          ),
         ],
       ),
     );
+  }
+}
+
+class imagenClick extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      InkWell(
+        onTap: () {},
+        child: Container(
+          // ignore: prefer_const_constructors
+          child: Image.asset('images/monstruo.png'),
+          height: 100,
+          width: 100,
+        ),
+      ),
+    ]);
   }
 }
 
